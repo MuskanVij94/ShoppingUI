cfec447bf7960119a9c79bdf94fb380d371825cb
Muskanvij <vijmuskan94@gmail.com>
2023-04-13 12:44:58 +0530

    Muskan_13/04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Immigration-Project';
+
+  social: any = {};
+
+  loader: boolean = true;
+
+  constructor(
+    // private dbService: DbService
+  ) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loader = false;
+      // this.getSocialUrl();
+    },100);
+  }
+  // getSocialUrl() {
+  //   let unsub = this.dbService.socialSubject.subscribe((value) => {
+  //     if(value !== null) {
+  //       this.social = { ...value };
+  //       this.dbService.getWindowRef().setTimeout(() => unsub.unsubscribe(), this.dbService.timeoutInterval * 6)
+  //     }
+  //   })
+  // }
+
 }
